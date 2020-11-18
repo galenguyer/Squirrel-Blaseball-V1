1aed762ad351792d2dacfa862f00531d1f0f989c
Galen Guyer <galen@galenguyer.com>
2020-11-18 11:08:29 -0500

    Initial commit

---
+++ Squirrel.Ingest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Squirrel.Ingest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
