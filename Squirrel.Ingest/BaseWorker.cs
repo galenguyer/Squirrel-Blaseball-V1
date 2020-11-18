f06d296cb39a728baa11b06dd2a42e8fca4f89fd
Galen Guyer <galen@galenguyer.com>
2020-11-18 11:52:54 -0500

    Add BaseWorker

---
+++ Squirrel.Ingest/BaseWorker.cs
@@ -0,0 +1,37 @@
+﻿// https://github.com/xSke/Chronicler/blob/main/SIBR.Storage.Ingest/BaseWorker.cs
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using System;
+using System.Threading.Tasks;
+
+namespace Squirrel.Ingest
+{
+    public abstract class BaseWorker
+    {
+        protected readonly ILogger _logger;
+
+        protected BaseWorker(IServiceProvider services)
+        {
+            _logger = services.GetRequiredService<ILogger>()
+                .ForContext(GetType());
+        }
+
+        protected abstract Task Run();
+
+        public async Task Start()
+        {
+            while (true)
+            {
+                try
+                {
+                    _logger.Information("Starting ingest worker {WorkerType}", GetType().Name);
+                    await Run();
+                }
+                catch (Exception e)
+                {
+                    _logger.Error(e, "Error while running worker {WorkerType}", GetType().Name);
+                }
+            }
+        }
+    }
+}
