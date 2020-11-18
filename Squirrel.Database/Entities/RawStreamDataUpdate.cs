abdbd486313b68931369426a0e03dacb6f197b49
Galen Guyer <galen@galenguyer.com>
2020-11-18 14:31:40 -0500

    Use unix time milliseconds for timestamps

--- Squirrel.Database/Entities/RawStreamDataUpdate.cs
+++ Squirrel.Database/Entities/RawStreamDataUpdate.cs
@@ -12,8 +12,8 @@ namespace Squirrel.Database.Entities
     public class RawStreamDataUpdate
     {
         [BsonId] public string Id;
-        [BsonElement("firstSeen")] public Instant FirstSeen;
-        [BsonElement("lastSeen")] public Instant LastSeen;
+        [BsonElement("firstSeen")] public long FirstSeen;
+        [BsonElement("lastSeen")] public long LastSeen;
         [BsonElement("payload")] public BsonValue Payload;
 
         public RawStreamDataUpdate(Instant timestamp, JsonElement payload)
@@ -21,8 +21,8 @@ namespace Squirrel.Database.Entities
             Id = JsonHash.HashHex(payload);
             Payload = BsonSerializer.Deserialize<BsonValue>(payload.GetRawText());
 
-            FirstSeen = timestamp;
-            LastSeen = timestamp;
+            FirstSeen = timestamp.ToUnixTimeMilliseconds();
+            LastSeen = timestamp.ToUnixTimeMilliseconds();
         }
     }
 }
