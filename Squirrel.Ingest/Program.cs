a1ac3a6c2e999181b845f7cb7242aa3e9e9ac5a9
Galen Guyer <galen@galenguyer.com>
2020-11-18 11:47:30 -0500

    Add Serilog and start building services

--- Squirrel.Ingest/Program.cs
+++ Squirrel.Ingest/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 
 namespace Squirrel.Ingest
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IServiceProvider services = new ServiceCollection()
+                .AddSerilog()
+                .BuildServiceProvider();
+
+            await HandleIngest(services);
+        }
+
+        private static async Task HandleIngest(IServiceProvider services)
+        {
+            var logger = services.GetRequiredService<ILogger>();
+            logger.Information("Starting Ingress Workers");
+            System.Threading.Thread.Sleep(10000);
         }
     }
 }
---
+++ Squirrel.Ingest/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Squirrel.Ingest
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSerilog(this IServiceCollection services)
+        {
+            var logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Async(async =>
+                {
+                    async.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}", restrictedToMinimumLevel: LogEventLevel.Debug);
+                })
+                .CreateLogger();
+            Log.Logger = logger;
+
+            services.AddSingleton<ILogger>(logger);
+            return services;
+        }
+    }
+}
